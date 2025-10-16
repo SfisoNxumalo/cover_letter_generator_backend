c502084ac0551e04f3c85222e89b57c3feb97a85
Sifiso Mawila <Sifiso@nebula.co.za>
2025-10-16 12:50:08 +0200

    Add route and install openAI and pdfparser dependency

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CoverLetterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/generate-cover-letter', [CoverLetterController::class, 'generate']);
